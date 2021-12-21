32853644a8c6f5944d7c8ab6ba910bfaf3d8389d
yusufalikaraca0 <kavunvesalatalik@gmail.com>
2021-12-22 00:52:31 +0300

    ucgen

---
+++ 070/070/Program.cs
@@ -0,0 +1,17 @@
+﻿void ucgen(int uzunluk) {
+    for (int satir = 1; satir <=uzunluk; satir++)
+    {
+        for (int bosluk = 0;bosluk < uzunluk-satir; bosluk++)
+        {
+            Console.Write(" ");
+        }
+        for (int govde = 1; govde <= satir; govde++)
+        {
+            Console.Write("* ");
+        }
+        Console.Write("\n");
+    }
+}
+Console.WriteLine("uzunluğu ne kadar olsun ? ");
+ucgen(int.Parse(Console.ReadLine()));
+
